056e2ee14e13ab72c77a4b71731ab4c42713b938
Wilson Gomez <wilsongg08@gmail.com>
2022-08-16 07:58:00 -0500

    Conexion del back y el front en el home

--- twitter_clone/Twitter/views.py
+++ twitter_clone/Twitter/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Profile, Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'twitter/newsfeed.html', context)
