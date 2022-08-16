e5b1a79a1ecd1409da6b6702f1efa58d75212a26
Wilson Gomez <wilsongg08@gmail.com>
2022-08-16 06:07:45 -0500

    Modelos Profile y Post creados, y se añade los primeros datos de prueba desde sitio admin

--- twitter_clone/Twitter/models.py
+++ twitter_clone/Twitter/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile (models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    bio = models.CharField(default = 'Hola Twitter', max_length = 100)
+    # image
+    
+    def __str__(self) :
+        return f'Perfil de {self.user.username}'
+    
+class Post(models.Model):
+    timestamp = models.DateTimeField(default = timezone.now)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='posts')
+    
+    class Meta:
+        ordering = ['-timestamp']
+        
+    def __str__(self):
+        return self.content
+
