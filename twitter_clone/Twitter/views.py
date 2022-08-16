eb7a9c12882c879fb8f308bb2aa1fcf50ec7b70f
Wilson Gomez <wilsongg08@gmail.com>
2022-08-16 08:29:21 -0500

    Creacion del sistema de registro

--- twitter_clone/Twitter/views.py
+++ twitter_clone/Twitter/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile, Post
+from .forms import UserRegisterForm
 
 def home(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'twitter/newsfeed.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    else: 
+        form = UserRegisterForm()
+    
+    context = {'form': form}
+    return render(request, 'twitter/register.html', context)
+
