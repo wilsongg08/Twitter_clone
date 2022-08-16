4da57b8ceaba2984ba199816dd95c4ee04c034c0
Wilson Gomez <wilsongg08@gmail.com>
2022-08-16 15:52:36 -0500

    Creacion del sistema de creacion y guardado de posts

--- twitter_clone/Twitter/views.py
+++ twitter_clone/Twitter/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import Profile, Post
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, PostForm
 
 def home(request):
     posts = Post.objects.all()
-    context = {'posts': posts}
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False) # De esta forma pausamos el guardado del post para poder asignar quien lo escribió
+            post.user = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+        
+    context = {'posts': posts, 'form': form}
     return render(request, 'twitter/newsfeed.html', context)
 
 def register(request):
